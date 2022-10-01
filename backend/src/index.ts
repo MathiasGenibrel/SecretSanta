62477eae778cd50c3dc2226e3d05a55c30b1efb5
NyckolasP <nicolas@allforweb.fr>
2022-10-01 14:28:06 +0200

    Fix route back & package

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,28 +10,26 @@ const jwt = require('jsonwebtoken')
 const app = express();
 
 app.get("/", (_req: Request, res: Response) => {
-  getAllUsers().then((data:any) => {
+  /*getAllUsers().then((data:any) => {
     let users:IUser[] = [...data]
     res.send(data);
-  })
+  })*/
+  res.send('/');
 });
 
 app.get("/register", (_req: Request, res: Response) => {
-  let newUser = new User('valenti@hotmail.fr','azerty','co')
+  res.send('register');
+});
+
+app.post("/register", (req: Request, res: Response) => {
+  let newUser = new User(req.body.email,req.body.password,req.body.pseudo)
   newUser.isExist()
   // newUser.create()
-  res.send('done');
+  res.send('register');
 });
 
 app.get("/login", (_req: Request, res: Response) => {
-  getConnect('nicolas@allforweb.fr','azerty').then((data:any) =>{
-    if (data){
-      const token = jwt.sign({username: data.pseudo, email: data.email, id: data.id }, environment.secretToken);
-      res.json(token);
-      /* const decoded = jwt.verify(token, environment.secretToken);
-      console.log(decoded)*/
-    }
-  })
+  res.send('login')
 });
 
 app.post("/login", (req: Request, res: Response) => {
@@ -39,6 +37,8 @@ app.post("/login", (req: Request, res: Response) => {
     if (data){
       const token = jwt.sign({username: data.pseudo, email: data.email, id: data.id }, environment.secretToken);
       res.json(token);
+      /* const decoded = jwt.verify(token, environment.secretToken);
+      console.log(decoded)*/
     }
   })
 });
