c1a08d6ae78c6c91687204aac5a77da6586affa5
MathiasGenibrel <mathias.geni@gmail.com>
2022-10-01 20:47:39 +0200

    🚚 Move Views Folders

--- frontend/src/views/Login.tsx
+++ frontend/src/views/Login.tsx
@@ -1,22 +1,26 @@
 import React, { useContext } from "react";
 
-// Import context
-import AuthContext from "../../context/auth-context";
-
 // Import components
-import { auth } from "../../helpers/auth";
-import { Form } from "../organism/Form/Form";
+import { Form } from "../components/organism/Form/Form";
 
+// Import styles
 import "./ConnectionForm.css";
 
+// Import context
+import AuthContext from "../context/auth-context";
+
+// Import auth
+import { auth } from "../helpers/auth";
+
 // TODO: Change path of auth function (in submitHandler)
-export const Register = () => {
+export const Login = () => {
   const user = useContext(AuthContext);
+
   return (
     <section>
       <Form
-        type="register"
-        title="Create an Account"
+        type="login"
+        title="Connect to your Account"
         submitHandler={(data) => auth(data, "users/1", user)}
       />
     </section>
--- frontend/src/views/Register.tsx
+++ frontend/src/views/Register.tsx
@@ -1,26 +1,22 @@
 import React, { useContext } from "react";
 
-// Import components
-import { Form } from "../organism/Form/Form";
+// Import context
+import AuthContext from "../context/auth-context";
 
-// Import styles
+// Import components
+import { auth } from "../helpers/auth";
+import { Form } from "../components/organism/Form/Form";
+
 import "./ConnectionForm.css";
 
-// Import context
-import AuthContext from "../../context/auth-context";
-
-// Import auth
-import { auth } from "../../helpers/auth";
-
 // TODO: Change path of auth function (in submitHandler)
-export const Login = () => {
+export const Register = () => {
   const user = useContext(AuthContext);
-
   return (
     <section>
       <Form
-        type="login"
-        title="Connect to your Account"
+        type="register"
+        title="Create an Account"
         submitHandler={(data) => auth(data, "users/1", user)}
       />
     </section>
